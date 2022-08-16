cfa0cd0a72fd6b48aa0d3e3187ff686140f51f29
ARSahad <arsahad7@gmail.com>
2022-08-16 13:15:30 +0600

    email password submit ok

---
+++ javaScript/login.js
@@ -0,0 +1,7 @@
+document.getElementById('submit-btn').addEventListener('click', function()
+{
+    const useremail = document.getElementById('user-email');
+    const email = useremail.value;
+    const userpassword = document.getElementById('user-password');
+    const password = userpassword.value;
+})
